5241fb35d939a274b1d0970f0d97b72be084a58e
unknown <danpereira3009@gmail.com>
2022-09-16 14:39:05 -0300

    remoção de veículo e calculod e horas

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -23,21 +23,27 @@ namespace DesafioFundamentos.Models
         public void RemoverVeiculo()
         {
             Console.WriteLine("Digite a placa do veículo para remover:");
-
+            string placaDigitada = Console.ReadLine();
             // Pedir para o usuário digitar a placa e armazenar na variável placa
             // *IMPLEMENTE AQUI*
             string placa = "";
+            placa = placaDigitada;
 
             // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
             {
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
-
+                decimal horas = 0;
+                
+                int horasPermaneciadas = Convert.ToInt32(Console.ReadLine());
+                horas = horasPermaneciadas;
                 // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
                 // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
                 // *IMPLEMENTE AQUI*
-                int horas = 0;
+                
+                
                 decimal valorTotal = 0; 
+                valorTotal = (precoInicial + (precoPorHora * horas));
 
                 // TODO: Remover a placa digitada da lista de veículos
                 // *IMPLEMENTE AQUI*
